4f9ec2e573897585deabc7d39035179ac5c4d6df
Alexander Klymuk <swcAlexander@gmail.com>
2024-10-31 21:11:26 +0200

    add .

--- src/app/pages/tools/page.tsx
+++ src/app/pages/tools/page.tsx
@@ -1,35 +1,44 @@
-'use client'
+'use client';
 import { useState } from 'react';
 import generateRandomNumber from '@/api/random/random';
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 import Form from '@/app/(components)/Forms/Form';
 
 const Tools = () => {
-    const toolList = ["Об'єднання PDF", "Роз'єднання PDF", "Зменшення PDF", "Конвертація PDF в JPEG", "Конвертація JPEG в PDF", "Порівняння EXEL"]
-    const [selectetTool, setSelectedTool] = useState<string>('')
-    const handleToolClick = (tool: string) => {
-        setSelectedTool(tool)
+  const toolList = [
+    "Об'єднання PDF",
+    "Роз'єднання PDF",
+    'Зменшення PDF',
+    'Конвертація PDF в JPEG',
+    'Конвертація JPEG в PDF',
+    'Порівняння EXEL',
+  ];
+  const [selectetTool, setSelectedTool] = useState<string>('');
+  const handleToolClick = (tool: string) => {
+    setSelectedTool(tool);
+  };
 
-    }
-    
-    return (
-        <>
-             <main className='container column'>
-                <h1 className="hidden">PDF Tools</h1>
-                <nav className={styles.nav}>
-                    <ul className={styles.nav_list}>
-                        {toolList.map(listEl => 
-                            <li key={generateRandomNumber(listEl.length)}><button type='button' onClick={() =>handleToolClick(listEl)} className={styles.list_button}>{listEl}</button></li>
-                        )}
-                    </ul>
-                </nav>
-
-                {selectetTool && <Form tools={selectetTool} />}
-
-
-            </main>
-        </>
-
-    )
-}
+  return (
+    <>
+      <main className="container column">
+        <h1 className="hidden">PDF Tools</h1>
+        <nav className={styles.nav}>
+          <ul className={styles.nav_list}>
+            {toolList.map((listEl) => (
+              <li key={generateRandomNumber(listEl.length)}>
+                <button
+                  type="button"
+                  onClick={() => handleToolClick(listEl)}
+                  className={styles.list_button}>
+                  {listEl}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Form tools={selectetTool} />
+      </main>
+    </>
+  );
+};
 export default Tools;
